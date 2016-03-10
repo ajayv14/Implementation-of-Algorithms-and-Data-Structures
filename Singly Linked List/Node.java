7249135d3c8139dfaac52e25118537c5faeb6f34
Ajay Vijayasarathy <ajay.v14@gmail.com>
2016-03-10 15:46:56 -0600

    Node
    Singly linked list Node Structure

---
+++ Singly Linked List/Node.java
@@ -0,0 +1,26 @@
+package DSpkg;
+
+ class Node {
+		 String data;
+	    Node next;
+	
+	public Node(String data){
+		this.data = data;
+		}
+	
+	
+	public Node(){
+		
+	}
+	
+	
+	@Override
+	public String toString(){
+		
+		return data;
+	}
+	
+	
+	
+	
+	}
