df924f2cf39e40a9d1721ae682f6e86f5f6a9d19
Ajay Vijayasarathy <ajay.v14@gmail.com>
2016-03-11 20:56:06 -0600

    Sorting algorithm implementations

---
+++ Sorting Algorithms/SelectionSort.java
@@ -0,0 +1,74 @@
+package SortingAlgorithms;
+
+public class SelectionSort {
+
+	
+	int[] arr={2,5,1,4,7,1,10};
+	int N = arr.length;
+
+	     public static void main(String []args){
+	       
+	     
+	         /*selection sort algo
+	            1-2-5-6-7-2
+	         n--length of arry
+	         min, i
+	                 for i 0---> n-1
+	          min = i;
+	        
+	         for j  i+1 ----->n-1 
+	        if arr[j]<min
+	             min = j;                 
+	              swap(i,j);
+	         
+	         */
+	         
+	        SelectionSort h = new SelectionSort();
+	          h.selectionSort();
+	       h.printArray();
+	         
+	         
+	         
+	     }
+	     
+	     
+	     public void selectionSort(){
+	         
+	         int min = 0;
+	         
+	          for(int i=0;i<N;i++){
+	             min = i;
+	          
+	              for(int j=i+1;j<N;j++){
+	                  
+	                  if(arr[j]<arr[min]){
+	                      min = j;
+	                 
+	                  }
+	                
+	              }
+	              
+	               swap(i,min);
+	          }
+	         
+	     }
+	     
+	     public void swap(int i,int j){
+	    
+	   int temp = arr[j];
+	    arr[j]= arr[i];
+	    arr[i]= temp;
+	    
+	    
+	}         
+	         
+	    public void printArray(){
+	        for(int i =0;i<arr.length;i++){
+	            
+	            System.out.println(arr[i]);
+	        }
+	        
+	    }     
+	    
+	     
+}
---
+++ Sorting Algorithms/quickSort.java
@@ -0,0 +1,65 @@
+package SortingAlgorithms;
+
+// pivot , Pindex
+
+public class quickSort {
+	static int[]  arr = {3,5,2,1,9,7,0};
+
+	public static void main(String[] args){
+	 				
+		quickSort q = new quickSort();
+		q.qSort(arr,0,arr.length-1);
+        
+	    q.printArray(arr);
+	
+	
+//q.swap(0,5);
+
+//q.printArray(arr);
+	
+	
+	}
+	
+	public void qSort(int[] arr, int startIndex, int endIndex){
+		
+		if(startIndex<=endIndex){
+		
+		int partitionIndex = partition(startIndex,endIndex);
+		qSort(arr,startIndex,partitionIndex-1);
+		qSort(arr,partitionIndex+1,endIndex);
+		
+		}
+		
+	}
+	
+	// [1, 4, 5, 6,7 ,8 ,9,3,4]
+public int partition(int startIndex,int endIndex){
+		
+int pivot = arr[endIndex];		
+		int pIndex = startIndex;
+for(int i=startIndex;i<= endIndex-1;i++){
+	
+	if(arr[i]<=pivot){		
+		swap(i,pIndex);
+		pIndex++;
+	}	
+}
+swap(pIndex,endIndex);
+		return pIndex;
+}
+	
+	
+	public void swap(int pIndex, int endIndex){
+		int tmp;
+	    tmp = arr[pIndex];
+		arr[pIndex] = arr[endIndex];
+		arr[endIndex]= tmp;
+}
+	
+	
+	public void printArray(int[] array){
+	for(int val : array){
+		System.out.println(val);
+	}
+	}
+}
