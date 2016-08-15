83a054d01eabce39d3ec70e342c09f7489f45e20
ajay <ajay.v14@gmail.com>
2016-08-14 23:28:56 -0700

    Added file BalancedBinaryTree.java

---
+++ Tree/BalancedBinaryTree.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class BalancedBinaryTree {
+	
+    public boolean isBalanced(TreeNode root) {
+        
+        return dfsDepth(root)!=-1;
+        
+    }
+    
+    
+    int dfsDepth(TreeNode root){
+        
+        if(root==null) return 0;
+        
+          int leftDepth =  dfsDepth(root.left);
+          
+          if(leftDepth==-1) return -1;
+    
+         
+          int rightDepth =  dfsDepth(root.right);
+          
+          if(rightDepth==-1) return -1;
+          
+          // diff btw left and right height > 1
+          if(Math.abs (leftDepth-rightDepth)>1) return -1;
+          
+          return Math.max(leftDepth,rightDepth)+1;
+          
+
+    }
+    
+    
+    
+}
