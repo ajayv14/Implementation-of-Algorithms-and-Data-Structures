e7aed8155cd3e57717bcc9c9a27fff348c43367d
ajayv14 <ajay.v14@gmail.com>
2017-09-15 00:25:02 -0700

    added - Intersection of two Linked lists

---
+++ Singly Linked List/IntersectionOfTwoLists.java
@@ -0,0 +1,28 @@
+public class IntersectionOfTwoLists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+   
+   /*     
+        HashMap<ListNode,Integer> hm = new HashMap<ListNode,Integer>();
+        
+        if(headA==null || headB==null)return null;
+        
+        ListNode i = headA;
+        ListNode j = headB;
+        
+        while(i!=null){
+          hm.put(i,i.val);
+          i = i.next;
+        }
+            
+        while(j!=null){
+            if(hm.containsKey(j)){
+               return j;
+            }
+            j=j.next;
+        }              
+        return null;              
+    /*
+
+
+    }
+}
