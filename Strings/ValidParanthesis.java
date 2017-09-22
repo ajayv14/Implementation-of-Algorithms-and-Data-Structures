a45a6ad7a7d0c5c768c6aa3cd0febbce4ab5e443
ajayv14 <ajay.v14@gmail.com>
2017-09-22 00:29:50 -0700

    added valid paranthesis

---
+++ Strings/ValidParanthesis.java
@@ -0,0 +1,17 @@
+class ValidParanthesis {
+    public boolean isValid(String s) {
+        
+        Stack<Character> stack = new Stack<>();
+        char[] ch = s.toCharArray();
+
+        for(char c : ch){            
+            if(c == '{') stack.push('}');            
+            else if(c=='(') stack.push(')');            
+            else if(c=='[') stack.push(']');            
+            else if(stack.isEmpty() || stack.pop() != c) return false;         
+        }     
+           
+        return stack.isEmpty();    
+        
+    }
+}
