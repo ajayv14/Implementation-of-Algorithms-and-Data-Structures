d6ca1e079721cdc8a82b8182ee855b7ba06a8c11
ajayvijayasarathy <ajay.v14@gmail.com>
2017-10-29 23:43:02 -0700

    added 217. Contains Duplicate

---
+++ Arrays/DuplicateInArray.java
@@ -0,0 +1,25 @@
+class DuplicateInArray {
+    public boolean containsDuplicate(int[] nums) {
+     
+        /*using hash set -- Time O(N) Space O(N)*/
+        
+        Set<Integer> distinct = new HashSet<>();
+        
+        
+        for(int n : nums){
+            
+            if(!distinct.contains(n)){
+            
+                distinct.add(n);
+            
+            }
+            
+            else return true;           
+            
+        }       
+        
+        return false;        
+        
+        
+    }
+}
