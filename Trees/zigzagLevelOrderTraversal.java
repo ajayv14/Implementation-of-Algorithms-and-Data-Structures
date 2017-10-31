ff8160ddd93add33080612aae0323b46ae2c9395
ajayvijayasarathy <ajay.v14@gmail.com>
2017-10-31 00:52:16 -0700

    added 314. Binary Tree Vertical Order Travers

---
+++ Trees/zigzagLevelOrderTraversal.java
@@ -0,0 +1,63 @@
+
+class zigzagLevelOrderTraversal {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        
+      
+        /*usual level order traversal*/
+      
+        boolean odd = true; /*to check if level is odd or even*/
+        
+        
+        /*return result list*/
+        
+        List<List<Integer>> result = new ArrayList<>();
+        
+        
+       if(root == null) return result; 
+        
+       Queue<TreeNode>  queue = new LinkedList<>();
+       
+       queue.add(root); 
+        
+       while(!queue.isEmpty()){
+           
+           List<Integer> list = new ArrayList<>(); /*temp list to hold all values in a level*/
+           
+           int level = queue.size();
+                      
+           for(int i = 0; i < level; i++){
+               
+               TreeNode node = queue.poll();
+            
+               
+               /*zig-zag, reverse add the elements to list in case of level is even*/
+               
+               if(odd){
+                   list.add(node.val);
+               }
+               
+               else{
+                   list.add(0,node.val);
+               }
+              
+               
+               if(node.left != null) queue.add(node.left);
+           
+               if(node.right!= null) queue.add(node.right);
+           
+           
+           }
+           
+           odd = odd?false:true; /*flip the odd value*/
+           
+           result.add(list); 
+                 
+           
+       } 
+        
+        
+        
+      return result;  
+        
+    }
+}
