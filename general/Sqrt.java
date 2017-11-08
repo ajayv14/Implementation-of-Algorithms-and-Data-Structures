92dcae23edc75f3ff6f91b987fa602939d229775
ajayvijayasarathy <ajay.v14@gmail.com>
2017-11-07 23:43:02 -0800

    added 69. Sqrt(x)

---
+++ general/Sqrt.java
@@ -0,0 +1,35 @@
+
+
+class Sqrt {
+    public int mySqrt(int x) {
+        
+        /* 0------------root x ------------------x */
+        
+        int start = 1;
+        int end = x;
+        int result = 0;
+        
+        while(start <= end){
+            
+            int mid = start + (end - start) / 2 ;
+            
+        
+            if(mid <= x / mid){
+              
+                start = mid + 1; 
+                result = mid;
+            }
+            
+            else {
+                
+                end = mid - 1;                
+                
+            }
+            
+                    
+        }
+        
+        return result;
+        
+    }
+}
