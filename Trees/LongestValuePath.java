a235fa2b06234937b53d994c95f669676071c485
ajayv14 <ajay.v14@gmail.com>
2019-03-13 16:23:40 -0700

    added soln for LongestValuePath

---
+++ Trees/LongestValuePath.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class LongestValuePath {
+    
+    int len = 0;
+    public int longestUnivaluePath(TreeNode root) {        
+       if(root != null) traverse(root,-1);
+        return len;
+    }
+    
+    
+    private int traverse(TreeNode root, int prevVal){
+        
+        if (root == null) return 0;
+        
+        int count = 0;
+        int left = traverse(root.left, root.val);
+        int right = traverse(root.right,root.val);
+        
+                
+        len = Math.max(len,left+right);
+        //System.out.println(len);
+        if(root.val == prevVal)
+            count = 1+Math.max(left,right);                
+                
+        return count;        
+    }    
+}
