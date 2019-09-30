7608af26cd9f0f79b84225c182ea5b959ec2c28f
ajayv14 <ajay.v14@gmail.com>
2019-09-30 12:52:31 -0700

    more efficient code on 257. leetcode Binary Tree Paths

--- Trees/BinaryTreePaths.java
+++ Trees/BinaryTreePaths.java
@@ -19,17 +19,37 @@ public class BinaryTreePaths {
     return allPath;    
         
     }
-    
-private void preTraverse(TreeNode root,String path,List<String> allPath){
-    
-if(root.left==null && root.right == null)  allPath.add(path+root.val);   
 
-if(root.left!=null)  preTraverse(root.left,path+root.val+"->",allPath);  
+   public void DFS(TreeNode root, String tempPath, List<String> allPaths ){
+        
+        tempPath += root.val;
+        
+        if(root.left != null){
+            DFS(root.left, tempPath + "->", allPaths);
+        }
+        
+        if(root.right != null){
+            DFS(root.right, tempPath + "->", allPaths);
+        }
+        
+        if(root.left == null && root.right == null){
+            allPaths.add(tempPath);
+            return;
+        }        
+    }
 
-if(root.right!= null)    preTraverse(root.right,path+root.val+"->",allPath); 
+
+   /*old method*/
+   private void preTraverseOld(TreeNode root,String path,List<String> allPath){
+    
+      if(root.left==null && root.right == null)  allPath.add(path+root.val);   
+
+      if(root.left!=null)  preTraverse(root.left,path+root.val+"->",allPath);  
+
+      if(root.right!= null)    preTraverse(root.right,path+root.val+"->",allPath); 
     
     
-}    
+   }    
     
     
     
