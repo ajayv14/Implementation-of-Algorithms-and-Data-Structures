676efcbac814671a0032cedbe745087716865b6b
ajayv14 <ajay.v14@gmail.com>
2019-09-30 12:45:48 -0700

    451 on leetcode, sort frequency updated with more efficient method

--- Strings/SortCharByfrequency.java
+++ Strings/SortCharByfrequency.java
@@ -1,5 +1,56 @@
+import java.util.*;
+/*451 on Leetcode */
 class SortCharByFrequency {
+
     public String frequencySort(String s) {
+        
+        int[] freq = new int[128]; //freq of char in the string
+        StringBuilder sb = new StringBuilder(); // to return string result  
+        
+        for(int i = 0; i < s.length(); i++){  
+            int ascii = (int)s.charAt(i);
+            freq[ascii]++;
+        }       
+
+        /*to hold <freq, list of characters> sorted by descending map */
+        Map<Integer, List<Character>> map = new TreeMap<>(Collections.reverseOrder());
+                        
+        for(int j = 0; j < freq.length; j++){
+            
+            
+            /*add the non zero elements to map with their respective freq as keys*/
+            if(freq[j] != 0){
+                
+                if(map.containsKey(freq[j])){
+                    List<Character> list = map.get(freq[j]);
+                    list.add((char)j);
+                    map.put(freq[j],list);
+                }
+                else{
+                    List<Character> list = new ArrayList<Character>();
+                    list.add((char)j);
+                    map.put(freq[j],list);    
+                }            
+            }            
+         }
+            
+        /*loop thro the reverse sorted keysets and build a string with number of occurences of characters*/
+        for(int n : map.keySet()){                  
+            List<Character> ls = map.get(n);
+            
+            for(char ch : ls){                
+                for(int repeat = 0; repeat < n; repeat++){
+                  sb.append(ch);  
+                }              
+            }
+        }      
+        
+        return sb.toString();
+    }
+
+  
+   /*Old approach*/
+    public String frequencySortOld(String s) {
     
      /*Step 1 - create a map and add all characters in the string*/
         
@@ -56,4 +107,13 @@ class SortCharByFrequency {
         
         
     }
+    
+    
+    public static void main(String[] args){
+      String s = "tree";
+      SortCharByFrequency obj = new SortCharByFrequency();
+      System.out.println(obj.frequencySort(s)); // expected o/p "eert"
+    }
+    
+    
 }
