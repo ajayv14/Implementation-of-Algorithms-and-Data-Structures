613bebe6d495b9cfc0e274231f8a6020bc62ae2e
ajayv14 <ajay.v14@gmail.com>
2019-12-10 19:03:38 -0800

    doubly linked list demo

---
+++ Doubly Linked List/DoublyLinkedListDemo.java
@@ -0,0 +1,102 @@
+class DoublyLinkedListDemo {
+   
+   static Node head = null;
+   int size = 0;      
+
+   public void addToFront(int val){
+   
+      Node node = new Node(val);
+      
+      if(head == null){
+         head = node;       
+      }
+      
+      else {      //if (head.next == null){
+         
+         head.prev = node;
+         node.next = head;
+         head = node;   
+      }
+   
+   }  
+   
+   public void addToEnd(int val){
+   
+       Node node = new Node(val);
+      
+      if(head == null ){
+         head = node;       
+      }
+      
+      else {      //if (head.next == null){
+         Node n = head;   
+         while(n.next != null){         
+         n = n.next;
+         } 
+         
+         n.next = node;
+         node.prev = n;                     
+      }            
+   }
+   
+   
+   public void addAfterNode(int val, int number){
+       
+       Node node = new Node(val);          
+              
+         if(head == null) return;     
+        
+         if(head.next == null && head.val == number){
+            
+            head.next = node;
+            node.prev = head;            
+         }
+         
+         else {
+         
+         Node n = head;   
+         while(n != null){
+         
+            if(n.val == number){
+                                                     
+               Node tempNext = n.next;
+               n.next = node;
+               node.prev = n;
+               node.next = tempNext;
+               tempNext.prev = node;           
+               return;
+            }         
+         n = n.next;       
+       }  
+
+         
+         
+         }         
+              
+           
+   }
+    
+    
+    
+   public static void main(String[] args){
+      
+      DoublyLinkedListDemo obj = new DoublyLinkedListDemo();
+         obj.addToFront(20);
+         obj.addToFront(10);
+         obj.addToEnd(30);
+         obj.addAfterNode(35,20);
+     
+      Node n = head;   
+      while(n != null){
+         System.out.println(n.val);
+         n = n.next;
+      }   
+      
+      
+   }      
+      
+
+
+
+
+}
---
+++ Doubly Linked List/Node.java
@@ -0,0 +1,14 @@
+// doubly linked list 
+class Node {
+   
+   int val;
+   Node next;
+   Node prev;
+   
+   
+   public Node(int val){
+      this.val = val;
+      Node prev = null;
+      Node next = null;
+   }  
+}
