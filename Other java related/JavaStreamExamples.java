0276b068dbccb3edc7cfc0897832a12f02ccd910
ajayv14 <ajay.v14@gmail.com>
2020-02-03 18:34:33 -0800

    starting with some basic examples of java streams

---
+++ Other java related/JavaStreamExamples.java
@@ -0,0 +1,51 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+class JavaStreamExamples {
+   
+   public static void main(String[] args){
+      
+      List<String> list1 = new ArrayList<>();
+      list1.add("AMD");
+      list1.add("Intel");
+      list1.add("Nvidia");
+      list1.add("Qualcomm");
+      
+      // forEach using Stream      
+      list1.stream().forEach(e -> System.out.println(e));
+      System.out.println(""); 
+      
+      //example2:
+      list1.stream().forEach(e ->{
+               System.out.println(e);
+               
+               if(e.equals("AMD")){
+                  System.out.println("cheers");
+               }      
+      
+      });
+      
+      System.out.println(""); 
+      
+      Map<Integer,String> map = new HashMap<>();
+      map.put(1,"Nvidia");
+      map.put(2,"AMD");
+      map.put(2,"Qualcomm");
+      map.put(3,"Intel");
+      
+            
+      //forEach on Map
+      map.forEach((k,v) -> System.out.println("key : " + k + " value : " + v));
+         
+      
+   
+   }
+
+
+
+
+
+
+}
