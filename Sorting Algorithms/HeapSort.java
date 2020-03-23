0bad5dad715c6664b9e9e2b9f837c2e85a10e544
ajayv14 <ajay.v14@gmail.com>
2020-03-22 21:30:43 -0700

    revisited minDepth and fixed bug in heapsort

--- Sorting Algorithms/HeapSort.java
+++ Sorting Algorithms/HeapSort.java
@@ -2,48 +2,77 @@
 // https://www.youtube.com/watch?v=WsNQuCa_-PU
 
 class HeapSort {
+      
+     /*logic : 1) Use heapify to build a max-heap--> max heap does not give sorted output, just satisfies property-- root(parent) > left & right child.
+
+               2) To overcome this, we swap last element and first element in array (largest element at index 0 goes to end). 
+               Now we need to exclude this(last element) from the heap, hence we use the end variable to limit the length of array.
+               Finally we heapify again from 0 -> end
+               (excluding the last element each run, iteratively)
+
+               exceptional cases to try: 
+               [5,1,1,2,0,0]
+      */    
+     
+      
 
      public void heapSort(int[] arr){
      
        int mid = arr.length/2 ;
        
+       /* build max-heap step- does not gurantee sorted order*/
        for(int i = mid; i >= 0; i--){
-            heapify(arr, i);
-       }        
+            heapify(arr,i, arr.length);
+       }
+       
+       /*sorting in ascending order*/
+       for(int i = nums.length - 1; i >=0; i--){
+            /*swap largest element (at index 0) with last index*/
+            swap(nums,0,i);
+            heapify(nums,0,i); // note- here we consider length of array to be i, as we dont wanna run the algo for entire length of array
+        }    
+       
+               
      
      }
      
-     private void heapify(int[] arr, int i){
+     private void heapify(int[] arr, int i, int end){  // end is to specify length of array manually
          
          int largest = i;
          int left = 2 * i + 1;
          int right = 2 * i + 2;
          
-         if(left < arr.length && arr[left] > arr[largest]){ // check bounds and compare values
+         if(left < end && arr[left] > arr[largest]){ // check bounds and compare values
             largest = left;
          }
          
-         if(right < arr.length && arr[right] > arr[largest]){
+         if(right < end && arr[right] > arr[largest]){
             largest = right;
          }
          
          if(largest != i){
             // perform swap arr[i] and arr[largest]
-            int temp = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = temp;
+             swap(arr,largest,i);
             //recursively fix the tree using heapify()
-            heapify(arr, largest);
+            heapify(arr,largest,end);
          }
          
      }
      
      
+      private void swap(int[] arr, int i, int j){
+        
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;        
+    }
+     
+     
      public static void main(String[] args){
          
-         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
+         //int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
          //int[] arr = {1, 2, 3, 4, 5, 5, 7};    
-         
+         int[] arr = {5,1,1,2,0,0};
          HeapSort obj = new HeapSort();
             obj.heapSort(arr);
             
--- Trees/MinimumDepth.java
+++ Trees/MinimumDepth.java
@@ -12,30 +12,22 @@
 /* find if a left or right child exists,
  *  calculate and return min of left and right depth at
  *  each node recursively  */
-public class MinumumDepth {
+public class MinimumDepth {
+    
     public int minDepth(TreeNode root) {
         
     //ckeck  null, min(left , right)        
+      int leftMin = 0, rightMin = 0;
       
-      int leftMin = 0;
-      int rightMin = 0;
-      
-       if(root == null) return 0;
+      if(root == null) return 0;
        
-       if(root.left != null){
-         leftMin = minDepth(root.left);  
-        } 
-        
-       if(root.right!=null){
-         rightMin = minDepth(root.right);
-       }
-        //Special case when root has just one child tree--[1,2]
-        if(leftMin == 0 || rightMin == 0){ 
-            
+      leftMin = minDepth(root.left);  
+      rightMin = minDepth(root.right);
+       
+      //Special case when root has just one child tree--[1,2]
+      if(leftMin == 0 || rightMin == 0){ 
             return 1+leftMin+rightMin;
-        }
-        
-        return (Math.min(leftMin,rightMin)+1);
-        
+      }        
+      return (Math.min(leftMin,rightMin)+1);        
     }
 }
