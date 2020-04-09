a434d407eff81f9a6fea3c9354be351cc53fd6c6
ajayv14 <ajay.v14@gmail.com>
2020-04-08 21:53:20 -0700

    added another approach for sum of subarray = k

--- Arrays/SubArraySumEqualsK.java
+++ Arrays/SubArraySumEqualsK.java
@@ -21,6 +21,35 @@ class SubArraySumEqualsK {
     }
     
     
+    //easy to understand soln:
+    
+    public int subarraySum(int[] nums, int k) {
+                   
+        int count = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            
+            int currentSum = nums[i];
+            
+            if(currentSum == k) count++;
+            
+            for(int j = i + 1; j < nums.length; j++){
+                
+                currentSum += nums[j];                
+                if(currentSum == k){
+                  count++; 
+                  //break;  // this is a good condition to keep, but [0,0,0,0,0,0,0,0,0,0] has 55 cont.sub arrays where sum = 0;
+                }                
+            }
+            
+        }
+       
+                
+        return count;
+        
+    }
+    
+    
     
     /*test*/
     
