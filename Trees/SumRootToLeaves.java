4f67270c240484e5fadd485f19e2b59c549e4cd6
ajayv14 <ajay.v14@gmail.com>
2020-06-26 10:07:32 -0700

    Added soln for Sum root to leaves

---
+++ Trees/SumRootToLeaves.java
@@ -0,0 +1,37 @@
+/**
+  * credits : https://leetcode.com/problems/sum-root-to-leaf-numbers
+  *
+**/
+
+class SumRootToLeaves {
+    
+    int totalSum = 0;
+    
+    public int sumNumbers(TreeNode root) {
+                      
+       dfs(root,"");    
+       return totalSum; 
+    }
+    
+    private void dfs(TreeNode root, String number){
+        
+        if(root == null) return;
+        
+        number += root.val + "";
+        
+        //if(root.left != null){
+            dfs(root.left, number);
+        //}
+        
+        //if(root.right != null){
+            dfs(root.right, number);
+        //}
+        
+        if(root.left == null && root.right == null){
+            totalSum += Integer.parseInt(number);
+        }
+    }
+}
+
+
+
