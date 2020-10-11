2d29c51d0327eb0de5f783ba1096ed21d2a2b83c
ajayv14 <ajay.v14@gmail.com>
2020-10-11 00:40:02 -0700

    added soln for max level sum in a tree, leetcode

---
+++ Trees/MaxLevelSum.java
@@ -0,0 +1,42 @@
+class MaxLevelSum {
+    public int maxLevelSum(TreeNode root) {
+        
+        //level order traversal
+        
+        if(root == null) return 0;
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        int largestLevelSum = Integer.MIN_VALUE;
+        int largestCorrespondingLevel = 0;
+        int level = 0;
+       
+        while(!queue.isEmpty()){
+                        
+            level++;
+            int size = queue.size();
+            int tempSum = 0;
+            
+            for(int i = 0; i < size; i++){                
+                
+                TreeNode node = queue.poll();
+                tempSum += node.val;
+                
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+            }
+            
+           // System.out.println(tempSum);
+            
+            if(tempSum > largestLevelSum){
+                
+                largestLevelSum = tempSum;
+                largestCorrespondingLevel = level;
+                
+            } 
+        }
+        
+        return largestCorrespondingLevel;
+    }
+}
