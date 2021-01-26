e6e4f42930015b89304e17a9a5b6fbc502f5810b
AVijayasarathy <ajay.vijayasarathy@internetbrands.com>
2021-01-25 19:23:18 -0800

    updated permutations & combinations

---
+++ Permutations and Combinations/Subsets.java
@@ -0,0 +1,46 @@
+
+
+//credit 1: https://leetcode.com/problems/subsets
+// credit 2: https://leetcode.com/problems/subsets/discuss/27281/a-general-approach-to-backtracking-questions-in-java-subsets-permutations-combination-sum-palindrome-partitioning
+
+
+// input [1,2,3] 
+//expected [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+// output [[],[1],[1,2],[1,2,3],[1,3],[2],[2,3],[3]]
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        
+        /*Final List to be returned*/
+        
+        List<List<Integer>> MainList = new ArrayList<>();       
+        //Arrays.sort(nums); not necessary, if order of subsets is not important
+        
+        /*backtracking helper function*/
+        
+        backtracking(MainList,nums, new ArrayList<>(),0); /*with start index 0*/
+        
+        return MainList;
+        
+    }
+    
+    
+    public void backtracking(List<List<Integer>> MainList, int[] nums, List<Integer> tempList, int start){
+        
+        MainList.add(new ArrayList<>(tempList)); /*initially add empty list*/
+        
+        /*run from start to nums.length - 1*/
+        for(int i = start;  i < nums.length; i++){
+            
+            tempList.add(nums[i]);
+            backtracking(MainList,nums,tempList,i+1);
+            tempList.remove(tempList.size() - 1); /*remove last element from list*/
+                     
+        }
+             
+        
+        
+    }   
+    
+    
+}
