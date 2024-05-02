9bbaf9fb00dd0bf4161a3e97ef63940678e647a7
Ajay <ajay.v14@gmail.com>
2024-05-01 23:17:28 -0700

    BFS soln for LC 75

--- AlgoApp/src/com/app/graph/KeysAndRooms.java
+++ AlgoApp/src/com/app/graph/KeysAndRooms.java
@@ -3,12 +3,14 @@ package com.app.graph;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Set;
 
+// LC 75
+// Checkout both BFS and DFS solution
 public class KeysAndRooms {
-
-    // 
 
     // Classic DFS solution
     public boolean canVisitAllRoomsDfs(List<List<Integer>> rooms) {
@@ -31,21 +33,49 @@ public class KeysAndRooms {
         }
     }
 
+    // Classic BFS solution Time : O(N + E) N -> Num of rooms (Nodes)  E -> total num of keys (Edges)
+    public boolean canVisitAllRoomsBFS(List<List<Integer>> rooms) {
+
+        Set<Integer> visited = new HashSet<>();
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        visited.add(0);
+
+        while (!queue.isEmpty()) {
+
+            int room = queue.remove();
+
+            for (Integer key : rooms.get(room)) { // for all keys in room
+
+                if (!visited.contains(key)) {
+                    visited.add(key);
+                    queue.add(key);
+                }
+            }
+        }
+        return (visited.size() == rooms.size()) ? true : false;
+    }
+
+
+
+
     public static void main(String[] args) {
-        
+
         KeysAndRooms obj = new KeysAndRooms();
 
-        List<List<Integer>> rooms1 = new ArrayList<List<Integer>>(Arrays.asList(Arrays.asList(1),Arrays.asList(2), Arrays.asList(3),Arrays.asList()));
+        List<List<Integer>> rooms1 = new ArrayList<List<Integer>>(
+                Arrays.asList(Arrays.asList(1), Arrays.asList(2), Arrays.asList(3), Arrays.asList()));
 
         System.out.println("Input 1 : [[1],[2],[3],[]]");
         System.out.println("Expected : true");
         System.out.println("Actual : " + obj.canVisitAllRoomsDfs(rooms1));
 
-        List<List<Integer>> rooms2 = new ArrayList<List<Integer>>(Arrays.asList(Arrays.asList(1,3),Arrays.asList(3,0,1), Arrays.asList(2),Arrays.asList(0)));
+        List<List<Integer>> rooms2 = new ArrayList<List<Integer>>(
+                Arrays.asList(Arrays.asList(1, 3), Arrays.asList(3, 0, 1), Arrays.asList(2), Arrays.asList(0)));
         System.out.println("Input 2 : [1,3],[3,0,1],[2],[0]]");
         System.out.println("Expected : false");
         System.out.println("Actual : " + obj.canVisitAllRoomsDfs(rooms2));
     }
 
-    
 }
