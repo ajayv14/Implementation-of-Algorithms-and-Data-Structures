32526502095f93ac383087e6418ae1de33bc6f96
Ajay <ajay.v14@gmail.com>
2024-08-26 20:27:32 -0700

    Updated LC 253 solution

--- AlgoApp/src/com/app/arrays2d/MeetingRoomsII.java
+++ AlgoApp/src/com/app/arrays2d/MeetingRoomsII.java
@@ -11,38 +11,26 @@ public class MeetingRoomsII {
     public int minMeetingRooms(int[][] intervals) {
 
         // Base case
-        if (intervals == null || intervals.length == 0)
-            return 0;
+        if (intervals == null || intervals.length == 0)  return 0;
 
         // Sort by intervals by start time - makes it easier to merger intervals later
         Arrays.sort(intervals, (x, y) -> x[0] - y[0]);
-
-        // minHeap to keep list of meetings that end earliest (by end time)
+        
         PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> Integer.compare(x[1], y[1]));
 
-        pq.offer(intervals[0]); // Add first interval end time to begin with
+        pq.offer(intervals[0]); // Add first interval to begin with
 
         for (int i = 1; i < intervals.length; i++) {
 
-            int startTime = intervals[i][0];
-            int endTime = intervals[i][1];
-
-            if (!pq.isEmpty() && startTime >= pq.peek()[1]) {
-
-                // No overlap detected
-                int[] prev = pq.poll();
-
-                // Update end time of current interval as it is larger.
-                prev[1] = endTime;
-                pq.offer(prev);
-
+            int[] interval = intervals[i];
+                      
+            if (interval[0] >= pq.peek()[1]) {
+                // No additional room required.
+                pq.poll(); 
             }
-
-            else {
-                // Merge is possible, hence need more rooms
-                pq.offer(intervals[i]);
-            }
-
+           
+            // Update pq with new time interval
+            pq.offer(intervals[i]);            
         }
 
         return pq.size();
@@ -54,11 +42,16 @@ public class MeetingRoomsII {
 
         int[][] intervals = new int[][] { { 0, 30 }, { 5, 10 }, { 15, 20 } };
 
-        CommonUtil.runExample("input : [0,30],[5,10],[15,20]", "2", obj.minMeetingRooms(intervals) + "");
+        CommonUtil.runExample("Example 1 : input : [0,30],[5,10],[15,20]", "2", obj.minMeetingRooms(intervals) + "");
 
         int[][] intervals2 = new int[][] { { 7, 10 }, { 2, 4 } };
 
-        CommonUtil.runExample("input : [7,10],[2,4] ", "1", obj.minMeetingRooms(intervals2) + "");
+        CommonUtil.runExample("Example 2 : input : [7,10],[2,4] ", "1", obj.minMeetingRooms(intervals2) + "");
+
+        int[][] intervals3 = new int[][] { { 13, 20 }, { 1, 13 } };
+
+        CommonUtil.runExample("Example 3 : input : [13,20],[1,13] ", "1", obj.minMeetingRooms(intervals3) + "");
+
     }
 
 }
