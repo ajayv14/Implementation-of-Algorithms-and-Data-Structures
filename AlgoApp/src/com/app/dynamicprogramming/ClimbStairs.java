dedc1c0982ddaf09b2f12106fe670d5961007dcf
Ajay <ajay.v14@gmail.com>
2024-09-07 22:29:51 -0700

    Cleanup old code.

--- AlgoApp/src/com/app/dynamicprogramming/ClimbStairs.java
+++ AlgoApp/src/com/app/dynamicprogramming/ClimbStairs.java
@@ -20,6 +20,24 @@ class ClimbStairs{
         }      
         return c;        
     }      
+
+
+
+     /*optimal cost depends on the previous step and the one before that*/
+
+     public int minCostClimbingStairs2(int[] cost) {
+                  
+      int[] dp = new int[cost.length];
+      
+      dp[0]=cost[0];
+      dp[1]=cost[1];
+      
+      for(int i=2;i<cost.length;i++){
+          dp[i]=cost[i] + Math.min(dp[i-1],dp[i-2]);
+      }        
+      return Math.min(dp[cost.length-1],dp[cost.length-2]);
+      
+  }
       
       public static void main(String[] args){
         ClimbStairs obj = new ClimbStairs();
