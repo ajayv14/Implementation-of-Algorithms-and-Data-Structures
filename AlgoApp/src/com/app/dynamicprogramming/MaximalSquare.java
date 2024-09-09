fcdb86fee43bb84875cb187a1f7ca9a2a9674eb9
Ajay <ajay.v14@gmail.com>
2024-09-08 17:11:48 -0700

    Custom soln for maximal square dp.

--- AlgoApp/src/com/app/dynamicprogramming/MaximalSquare.java
+++ AlgoApp/src/com/app/dynamicprogramming/MaximalSquare.java
@@ -1,54 +1,68 @@
 package com.app.dynamicprogramming;
 
 class MaximalSquare {
-     public int maximalSquare(char[][] matrix) {
+     
+
+    public int maximalSquare(char[][] matrix) {
+
+        // LC 221
+        // https://leetcode.com/problems/maximal-square
         
         /*DP non optimized-- https://www.youtube.com/watch?v=_Lf1looyJMU Thushar Roy*/
-        
+
+        /* Intution - Take current cell, it makes a square of 1 X 1.
+            How can you create a square larger, like 2 x 2 ? - You need '1' at top, left and top-left diagonal.
+            Or bottom, right and bottom-right cell. But for programming, former is easier.
+
+            But when we start with cell (0,0, we don't have top, left and diagonal cell, hence we pad with '0's on row and col. 
+         */    
+
+        /* Padding row and col with 0 is the easiest way to code without bothering abt boundaries. 
+         *  
+
+         Runtime O(m * n) :
+         Space : O(m * n)
+         */      
         
         /*base case*/
-        if(matrix.length <= 0 || matrix[0].length <=0) return 0;
+        if(matrix.length == 0 || matrix[0].length == 0) return 0;          
+       
+        int[][] dp = new int[matrix.length + 1][matrix[0].length + 1];
         
-        int m = matrix.length;
-        int n = matrix[0].length;
+        int max = 0;
         
-        int maxDimSquare = 0;
-        
-        //m + 1 and n + 1 to pad matrix with zeros on top - row and left - column
-        
-        int[][] dp = new int[m + 1][n + 1];
-        
-        
-        for(int i = 1; i < m + 1; i++){
+        for(int row = 1; row < matrix.length + 1; row++){
             
-            for(int j = 1; j < n + 1; j++){
-                
-                
-                if(matrix[i - 1][j - 1] == '1'){ // check only for 1s
-                                     
-                    
-                    // take minimum of left, top, top - left
-                    
-                    dp[i][j] = Math.min(dp[i][j - 1], Math.min(dp[i - 1][j - 1], dp[i - 1][j])) + 1;
-                    
-                    maxDimSquare = Math.max(maxDimSquare, dp[i][j]);
-                    
-                }                
-                
-                
+            for(int col = 1; col < matrix[0].length + 1; col++){
+
+               
+                // Referring to original matrix. Don;t have to calculate for '0'    
+                if(matrix[row - 1][col - 1] == '1') {
+
+                    int left = dp[row][col - 1];
+
+                    int top = dp[row - 1][col];
+
+                    int diagonal = dp[row - 1][col - 1];
+                   
+                    dp[row][col] = 1 + Math.min(diagonal, Math.min(left,top));     
+
+                    max = Math.max(max, dp[row][col]);             
+                }                           
+
+               
+
+               // printArray(dp);
+              
+                      
             }            
             
-        }
-        
-       // to check  printArray(dp);
-        
-        
-        return maxDimSquare * maxDimSquare;
-        
+        }           
+        return max * max;  
         
     }
     
- /*     
+     
  public static void printArray(int[][] a){
      
      
@@ -61,13 +75,13 @@ class MaximalSquare {
              
          }
          
-         System.out.println(" ");
+         System.out.println("--------");
+         
          
      }
      
      
  }   
    
-*/   
    
 }
