bb447a87f3b2917307773a0f66d84e6bb73f0d6c
Ajay <ajay.v14@gmail.com>
2024-10-06 17:41:43 -0700

    O(N) soln with hashmap for subarraySum=k

--- AlgoApp/src/com/app/arrays/SubArraySumEqualsK.java
+++ AlgoApp/src/com/app/arrays/SubArraySumEqualsK.java
@@ -1,32 +1,49 @@
 package com.app.arrays;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import com.app.common.CommonUtil;
 
 // https://leetcode.com/problems/subarray-sum-equals-k/description/
 // LC 560
 class SubArraySumEqualsK {
-    public int subarraySum(int[] nums, int k) {
+    
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1);
+    /*
+    Logic - Store prefix sum at each index. 
+    Now find if current prefixSum - K is present in the stored prefixSums.
+    
+    sum(nums[i,j] = prefixSum[j] -  prefixSum[i - 1])
+    prefixSum[i - 1] = prefixSum[j] - k; 
+  */
 
-        int sum = 0;
-        int result = 0;
+// Time and space : O(N)
+  public int subarraySum(int[] nums, int k) {
 
-        for (int i = 0; i < nums.length; i++) {
+    // PrefixSum - frequency
+    Map<Integer, Integer> map = new HashMap<>();
 
-            sum += nums[i];
+    int prefixSum = 0, ans = 0;
 
-            if (map.containsKey(sum - k)) { // subarray is found
-                result += map.get(sum - k);
-            }
+    // imagine a prefix sum array. Starts with 0 outside.
+    map.put(0,1); 
 
-            map.put(sum, map.getOrDefault(sum, 0) + 1); // if not present in hashmap, will get 0
-        }
-        return result;
+    for(int num : nums){
+
+        prefixSum += num;
+
+        // Since we have prefix sum, find if a number prefixSum - k exists, so it satisifies equation or find missing complementing the equation.
+
+        ans += map.getOrDefault(prefixSum - k, 0);
+
+        // update prefix sum count
+        map.put(prefixSum, map.getOrDefault(prefixSum,0) + 1);
     }
+
+    return ans;
+
+  }
 
 
 
@@ -62,16 +79,16 @@ class SubArraySumEqualsK {
 
         int[] input1 = new int[]{1,1,1};
         int k1 = 2;
-        CommonUtil.runExample("1,1,1","2", obj.subarraySum2(input1, k1) + "");
+        CommonUtil.runExample("1,1,1","2", obj.subarraySum(input1, k1) + "");
 
 
         int[] input2 = new int[]{1,2,3};
         int k2 = 3;
-        CommonUtil.runExample("1,2,3","2", obj.subarraySum2(input2, k2) + "");
+        CommonUtil.runExample("1,2,3","2", obj.subarraySum(input2, k2) + "");
 
         int[] input3 = new int[]{0,0,0,0,0,0,0,0,0,0};
         int k3 = 0;
-        CommonUtil.runExample("0,0,0,0,0,0,0,0,0,0", "55", obj.subarraySum2(input3, k3) + "");
+        CommonUtil.runExample("0,0,0,0,0,0,0,0,0,0", "55", obj.subarraySum(input3, k3) + "");
 
 
     }
