0483315ef6361ab19e52a6f4f178c59b10f34a82
Ajay <ajay.v14@gmail.com>
2024-10-09 12:15:22 -0700

    Dp soln for maximal square without unnecessary padding.

--- AlgoApp/src/com/app/dynamicprogramming/MaximalSquare.java
+++ AlgoApp/src/com/app/dynamicprogramming/MaximalSquare.java
@@ -2,86 +2,48 @@ package com.app.dynamicprogramming;
 
 class MaximalSquare {
      
-
-    public int maximalSquare(char[][] matrix) {
-
+   
         // LC 221
         // https://leetcode.com/problems/maximal-square
         
-        /*DP non optimized-- https://www.youtube.com/watch?v=_Lf1looyJMU Thushar Roy*/
+        /*DP non optimized-- https://www.youtube.com/watch?v=_Lf1looyJMU Thushar Roy
 
-        /* Intution - Take current cell, it makes a square of 1 X 1.
-            How can you create a square larger, like 2 x 2 ? - You need '1' at top, left and top-left diagonal.
-            Or bottom, right and bottom-right cell. But for programming, former is easier.
-
-            But when we start with cell (0,0, we don't have top, left and diagonal cell, hence we pad with '0's on row and col. 
-         */    
-
-        /* Padding row and col with 0 is the easiest way to code without bothering abt boundaries. 
-         *  
-
+        
          Runtime O(m * n) :
          Space : O(m * n)
          */      
         
-        /*base case*/
-        if(matrix.length == 0 || matrix[0].length == 0) return 0;          
-       
-        int[][] dp = new int[matrix.length + 1][matrix[0].length + 1];
+         public int maximalSquare(char[][] matrix) {             
         
-        int max = 0;
-        
-        for(int row = 1; row < matrix.length + 1; row++){
-            
-            for(int col = 1; col < matrix[0].length + 1; col++){
-
-               
-                // Referring to original matrix. Don;t have to calculate for '0'    
-                if(matrix[row - 1][col - 1] == '1') {
-
-                    int left = dp[row][col - 1];
-
-                    int top = dp[row - 1][col];
-
-                    int diagonal = dp[row - 1][col - 1];
-                   
-                    dp[row][col] = 1 + Math.min(diagonal, Math.min(left,top));     
-
-                    max = Math.max(max, dp[row][col]);             
-                }                           
-
-               
-
-               // printArray(dp);
-              
-                      
-            }            
-            
-        }           
-        return max * max;  
-        
-    }
+            int max = 0;
     
-     
- public static void printArray(int[][] a){
-     
-     
-     for(int i = 0; i < a.length; i++){
-
-         for(int j = 0; j < a[0].length; j++){
-             
-             
-             System.out.print(a[i][j] + " ");
-             
-         }
-         
-         System.out.println("--------");
-         
-         
-     }
-     
-     
- }   
-   
+            int[][] dp = new int[matrix.length][matrix[0].length];
+    
+            for(int row = 0; row < matrix.length; row++){
+                for(int col = 0; col < matrix[0].length; col++){
+    
+                    if(matrix[row][col] == '0') continue; // skip           
+                     
+                    // Cells that have left, right and diagonal cell
+                    if(row > 0 && col > 0){
+                           
+                        // 1 + min(diagonal, min(left and top))    
+                        dp[row][col] = 1 + Math.min(dp[row - 1][col - 1], Math.min(dp[row - 1][col], dp[row][col - 1]));       
+    
+                    }
+    
+                    // Cells at left or top wall
+                    else if(row == 0 || col == 0){
+                        dp[row][col] = 1;
+                    }
+    
+                    max = Math.max(max, dp[row][col]);
+                }
+            }      
+    
+    
+            return max * max;  
+            
+        }  
    
 }
