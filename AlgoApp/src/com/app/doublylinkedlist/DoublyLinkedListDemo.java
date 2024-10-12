d4eb44d9596c8ddcf653affcd6842cdf0f6b2771
Ajay <ajay.v14@gmail.com>
2024-10-11 22:25:11 -0700

    Basic DoubleLInkedList Impl.

--- AlgoApp/src/com/app/doublylinkedlist/DoublyLinkedListDemo.java
+++ AlgoApp/src/com/app/doublylinkedlist/DoublyLinkedListDemo.java
@@ -1,104 +1,146 @@
+package com.app.doublylinkedlist;
+
 //credits: https://www.youtube.com/watch?v=LvUgew66zOQ
 
 class DoublyLinkedListDemo {
-   
-   static Node head = null;
-   int size = 0;      
 
-   public void addToFront(int val){
-   
-      Node node = new Node(val);
-      
-      if(head == null){
-         head = node;       
-      }
-      
-      else {      //if (head.next == null){
-         
-         head.prev = node;
-         node.next = head;
-         head = node;   
-      }
-   
-   }  
-   
-   public void addToEnd(int val){
-   
-       Node node = new Node(val);
-      
-      if(head == null ){
-         head = node;       
-      }
-      
-      else {      //if (head.next == null){
-         Node n = head;   
-         while(n.next != null){         
-         n = n.next;
-         } 
-         
-         n.next = node;
-         node.prev = n;                     
-      }            
+   int size;
+   DLLNode head;
+   DLLNode tail;
+   int maxSize;
+
+   public DoublyLinkedListDemo(int k) {
+       this.size = 0;
+       this.maxSize = k;
    }
    
-   
-   public void addAfterNode(int val, int number){
+   public boolean insertFront(int value) {
+
+       if(isFull()) return false;
+
+       DLLNode node = new DLLNode(value);
+
+       if(head == null) head = tail = node;
+
+       else {
+
+           node.next = head;
+           head.prev = node;
+           head = node;                    
+       }
+
+       size++;
+
+       return true;
        
-       Node node = new Node(val);          
-              
-         if(head == null) return;     
-        
-         if(head.next == null && head.val == number){
-            
-            head.next = node;
-            node.prev = head;            
-         }
-         
-         else {
-         
-         Node n = head;   
-         while(n != null){
-         
-            if(n.val == number){
-                                                     
-               Node tempNext = n.next;
-               n.next = node;
-               node.prev = n;
-               node.next = tempNext;
-               tempNext.prev = node;           
-               return;
-            }         
-         n = n.next;       
-       }  
-
-         
-         
-         }         
-              
-           
    }
-    
-    
-    
+   
+   public boolean insertLast(int value) {
+
+       if(isFull()) return false;
+
+       DLLNode node = new DLLNode(value);
+
+       if(head == null) head = tail = node;
+
+       // Redundant condition head == tail
+
+       else {
+           tail.next = node;
+           node.prev = tail;
+           tail = node;            
+       }
+        size++;    
+
+        return true;    
+   }
+   
+   public boolean deleteFront() {
+
+       if(isEmpty()) return false;
+
+       if(head == tail) head = tail = null;
+
+       else {
+
+           head = head.next;
+           head.prev = null;
+       }
+
+       size--; 
+
+       return true;
+
+       
+   }
+   
+   public boolean deleteLast() {
+
+       if(isEmpty()) return false;
+
+       if(head == tail) head = tail = null;
+
+       else {
+           tail = tail.prev;
+           tail.next = null;
+       }
+
+       size--; 
+
+       return true;
+       
+   }
+   
+   public int getFront() {
+       
+       if(isEmpty()) return -1;
+
+       else return head.value;
+   }
+   
+   public int getRear() {
+
+       if(isEmpty()) return -1;
+
+       else return tail.value;    
+       
+   }
+   
+   public boolean isEmpty() {
+       return size == 0;             
+   }
+   
+   public boolean isFull() {
+       return size == maxSize;        
+   }
+
+
+   class DLLNode {
+
+       int value;
+       DLLNode next;
+       DLLNode prev;
+
+       public DLLNode(int value){            
+           this.value = value;
+       }
+   }
+
+
+
    public static void main(String[] args){
       
-      DoublyLinkedListDemo obj = new DoublyLinkedListDemo();
-         obj.addToFront(20);
-         obj.addToFront(10);
-         obj.addToEnd(30);
-         obj.addAfterNode(35,20);
-     
-      Node n = head;   
-      while(n != null){
-         System.out.println(n.val);
-         n = n.next;
-      }   
-      
-      
-   }      
-      
-
-
-
-
+      DoublyLinkedListDemo obj = new DoublyLinkedListDemo(10);
+         obj.insertFront(20);
+         obj.insertFront(10);
+         obj.insertLast(30);
+         //obj.addAfterNode(35,20);     
+   }     
 }
+
+   
+    
+    
+      
+
+
