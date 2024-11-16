24502c491bbc64c9d3c1680b52106f8aa743208e
Ajay <ajay.v14@gmail.com>
2024-11-15 22:05:36 -0800

    Clean soln for connecting cities with min cost - krushkal's algo.

---
+++ AlgoApp/src/com/app/graph/ConnectingCitiesMinCost.java
@@ -0,0 +1,90 @@
+package com.app.graph;
+
+import java.util.Arrays;
+//https://leetcode.com/problems/connecting-cities-with-minimum-cost/
+//1135. Connecting Cities With Minimum Cost
+
+public class ConnectingCitiesMinCost {
+
+     //Kruskal's Algorithm to create MST
+    public int minimumCost(int n, int[][] connections) {
+
+
+        int minCost = 0;
+
+        // Helps in implementing krushkals MST
+        UnionFind uf = new UnionFind(n);    
+
+        // Differs from Prim by sorting edges by weights
+        Arrays.sort(connections, (c1,c2)->Integer.compare(c1[2],c2[2]));    
+        
+        int edgeCount = 0;
+
+        for(int[] conn : connections){
+
+            int city1 = conn[0];
+            int city2 = conn[1];
+            int weight = conn[2];
+
+            if(uf.union(city1,city2)){
+                minCost += weight;
+                edgeCount++;
+            }
+        }
+        
+        return edgeCount == n - 1 ? minCost : -1;
+
+        
+
+    }
+
+    // Standard Union Find approach
+    class UnionFind {
+
+        int[] parent;
+        int[] rank;
+
+        public UnionFind(int n){
+            parent = new int[n + 1];
+            rank = new int[n + 1];
+
+            for(int i = 0; i < n + 1; i++){
+                parent[i] = i;
+            }
+        }
+               
+
+        public int find(int i){
+
+            if(parent[i] != i) parent[i] = find(parent[i]);
+            return parent[i];
+        }
+
+        public boolean union(int x, int y){
+
+            int rootX = find(x);
+            int rootY = find(y);
+
+            if(rootX == rootY) return false;
+
+            else if(rank[rootX] > rank[rootY]){
+                parent[rootY] = rootX;
+            }
+
+            else if(rank[rootY] > rank[rootX]){
+                parent[rootX] = rootY;
+            }
+
+            else if(rank[rootX] == rank[rootY]){
+                parent[rootX] = rootY;
+                rank[rootX]++;
+            }
+
+            return true;
+        }
+
+
+
+    }
+
+}
