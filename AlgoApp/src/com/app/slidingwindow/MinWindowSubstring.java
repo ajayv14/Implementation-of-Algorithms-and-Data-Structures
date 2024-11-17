006aa7a7d06ed5f09ca77af31fd79f72bf49e29b
Ajay <ajay.v14@gmail.com>
2024-11-16 21:27:29 -0800

    Clean soln for MinWindowSubstring

--- AlgoApp/src/com/app/slidingwindow/MinWindowSubstring.java
+++ AlgoApp/src/com/app/slidingwindow/MinWindowSubstring.java
@@ -1,10 +1,10 @@
-package com.app.strings;
+package com.app.slidingwindow;
 
 import java.util.HashMap;
 import java.util.Map;
 
 
-//LC 76 Hard
+//LC 76 Hard Minimum Window Substring
 
 // https://leetcode.com/problems/minimum-window-substring/
 
@@ -12,50 +12,37 @@ public class MinWindowSubstring {
 
 
     public String minWindow(String s, String t) {
-        if (t.length() > s.length()) {
-            return "";
-        }
 
+        if (t.length() > s.length()) return "";
+        
         // Frequency of occurence map
         Map<Character, Integer> freq = new HashMap<Character, Integer>();
 
         // initialize map
-        for (int i = 0; i < t.length(); i++) {
-            freq.put(t.charAt(i), freq.getOrDefault(t.charAt(i), 0) + 1);
+        for (char c : t.toCharArray()) {
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
         }
 
-        int count = 0, left = 0, right = 0, minWindow = Integer.MAX_VALUE;
+        int matches = 0, left = 0, right = 0, minWindow = Integer.MAX_VALUE;
         
         int start = 0;
-
        
-        /*
-        It enters a loop that moves the right pointer to the right. For each character it encounters, it decrements its 
-               frequency in the freq map and increments count if the frequency becomes positive.
-               Once count becomes equal to the length of string t, it means we have found a window that contains all characters of 
-               t. 
-               
-               It then enters a inner loop that moves the left pointer to the right to shrink the window. For each character it
-                encounters, it increments its frequency in the freq map and decrements count if the frequency becomes positive.
-                
-                If the length of the current window is less than minWindow, it updates minWindow and start.
-       
-        * */
 
-        for( right = 0 ; right < s.length(); right++) {
+        for(right = 0 ; right < s.length(); right++) {
 
-            char cur = s.charAt(right);
+            char rightChar = s.charAt(right);
             
-            if (freq.containsKey(cur)){
+            //Decrement freq and increase count if a valid char is found
+            if (freq.containsKey(rightChar)){
+                
+                freq.put(rightChar, freq.get(rightChar) - 1);
 
-                  
-
-                if(freq.get(cur) > 0) count++;
+                if(freq.get(rightChar) >= 0) matches++;
 
             } 
                                     
-            // Substrign found where all characters in String t are present.                                   
-            while (count == t.length()) {
+            // Substring window found where all characters in String t are present. Try shrinking window now.  
+            while (matches == t.length()) {
 
                 if(minWindow > right - left + 1){
 
@@ -69,18 +56,34 @@ public class MinWindowSubstring {
                 if(freq.containsKey(leftCur)){
 
                      freq.put(leftCur, freq.get(leftCur) + 1);
-                      if(freq.get(leftCur) > 0) count--;    
-                }                 
+                     
+                     if(freq.get(leftCur) > 0) matches--;    
+                }                
 
                 left++;          
-            
-            }   
-
-
-            if(freq.containsKey(cur)) freq.put(cur, freq.get(cur) - 1);                   
+            }              
         }
 
         return minWindow == Integer.MAX_VALUE ? "" : s.substring(start, start + minWindow);
     }
 
+
+
+        /*
+            Pre-populate map with character count in t.
+            
+            For each character encountered, if present in the freq map, decrement freq and increment matches count.
+
+            Once count becomes equal to the length of string t, it means we have found a window that contains all characters of t. 
+               
+            Now try shrinking this window by moving left pointer to right. Mark current pos of left pointer using start. For each character it
+            encounters, it increments its frequency in the freq map and decrements matches count if the frequency becomes positive.
+              
+            Shrink example : 
+
+            s = "ADOBACODEBANC", t = "ABC"
+
+            initial match - ADOBAC
+            shrink match - BAC    
+        * */
 }
