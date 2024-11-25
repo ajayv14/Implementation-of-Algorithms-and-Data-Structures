777fe4da9f7106d431f92dc4eeb040c40e059742
Ajay <ajay.v14@gmail.com>
2024-11-25 07:03:26 -0800

    Re-implemented Trie using HashMap based TrieNode.

--- AlgoApp/src/com/app/trie/Trie.java
+++ AlgoApp/src/com/app/trie/Trie.java
@@ -1,21 +1,111 @@
 package com.app.trie;
 
+import java.util.Map;
+import java.util.TreeMap;
 
-// Use hasmap to store trienode children ??
+//https://leetcode.com/problems/implement-trie-prefix-tree/
+// 208. Implement Trie (Prefix Tree)
+
+// Simplified TrieNode using HashMap
 
 class Trie {
 
     TrieNode root;
     
     /** Initialize your data structure here. */
-    public Trie() {       
+    public Trie() { 
         root = new TrieNode();        
     }
     
     /** Inserts a word into the trie. */
     public void insert(String word) {
         
-        /*pointer to root TrieNode*/
+        TrieNode cur = root;
+        
+        for(char ch : word.toCharArray()){
+            
+            cur.subNode.putIfAbsent(ch, new TrieNode());
+            
+            cur = cur.subNode.get(ch); // set new trie node as cur         
+        }      
+        
+        cur.isComplete = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+                
+        TrieNode cur = root;
+        
+        for(char c : word.toCharArray()){
+            
+            if(cur.subNode.get(c) == null) return false;            
+            cur = cur.subNode.get(c); // go one level deeper            
+        }        
+               
+        return cur.isComplete;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        
+        TrieNode cur = root;
+        
+        for(char c : prefix.toCharArray()){
+            
+            if(cur.subNode.get(c) == null) return false;            
+            cur = cur.subNode.get(c); // go one level deeper            
+        }  
+              
+        return true;
+    }
+    
+    
+    
+    static class TrieNode {
+        
+        Map<Character,TrieNode> subNode = new TreeMap<>();
+        boolean isComplete;        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+
+
+
+
+
+
+
+
+/*
+class Trie {
+
+    TrieNode root;
+    
+    // Initialize your data structure here.
+    public Trie() {       
+        root = new TrieNode();        
+    }
+    
+    // Inserts a word into the trie. 
+    public void insert(String word) {
+        
+        // pointer to root TrieNode
         TrieNode cur = root;
         
         for(char ch : word.toCharArray()){
@@ -31,7 +121,7 @@ class Trie {
         cur.setIsWordComplete(true); // mark word as complete
     }
     
-    /** Returns if the word is in the trie. */
+    // Returns if the word is in the trie. 
     public boolean search(String word) {
                 
         TrieNode cur = root; // get reference to root Trie Node
@@ -45,8 +135,8 @@ class Trie {
         return cur.getIsWordComplete();
     }
     
-    /** Returns if there is any word in the trie that starts with the given prefix. */
-    /* essentially same implementation a search function, but does not check for comepleteness of a word*/
+    // Returns if there is any word in the trie that starts with the given prefix. 
+    // essentially same implementation a search function, but does not check for comepleteness of a word
     public boolean startsWith(String prefix) {
         
         TrieNode cur = root;
@@ -95,7 +185,7 @@ class Trie {
     
     
     
-}
+} */
 
 /**
  * Your Trie object will be instantiated and called as such:
