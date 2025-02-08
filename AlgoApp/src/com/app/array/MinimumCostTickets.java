b5cd8b3d5c5571e07160bbd31a75c75aba35c20d
Ajay <ajay.v14@gmail.com>
2025-02-07 22:23:04 -0800

    Solved min cost to trip problem

---
+++ AlgoApp/src/com/app/array/MinimumCostTickets.java
@@ -0,0 +1,36 @@
+package com.app.array;
+
+public class MinimumCostTickets {
+
+    
+
+    public int findMinCost(int[] departing, int[] returning) {
+
+        int minDepCost = departing[0];
+        int minTripCost = Integer.MAX_VALUE;
+        
+
+        for(int i = 1; i < departing.length; i++){
+
+            minDepCost = Math.min(minDepCost, departing[i]);
+
+            minTripCost = Math.min(minTripCost, minDepCost + returning[i]); // i + 1 -> can't return on same day
+
+        }
+
+        return minTripCost;
+        
+    }
+    
+    public static void main(String[] args) {
+
+        int[] departing = {1, 2, 3, 4};
+        int[] returning = {4, 3, 2, 1};
+        
+        MinimumCostTickets obj = new MinimumCostTickets();
+
+        int cost = obj.findMinCost(departing, returning);
+        
+        System.out.println("Expected : 2 " + "actual : " + cost);
+    }
+}
