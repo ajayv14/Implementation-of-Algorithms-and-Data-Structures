ce02e99d42148fd6462575d9c75961d4456dde5d
Ajay <ajay.v14@gmail.com>
2025-04-16 21:20:52 -0700

    Clean soln for valid anagram check. LC 242.

--- AlgoApp/src/com/app/strings/ValidAnagram.java
+++ AlgoApp/src/com/app/strings/ValidAnagram.java
@@ -1,27 +1,24 @@
 class ValidAnagram {
+    
+// LC 242 https://leetcode.com/problems/valid-anagram/
+
     public boolean isAnagram(String s, String t) {
-        
-               
-        int[] Alphabet = new int[26];
-                
-               
-        char[] sArr = s.toCharArray();
-        char[] tArr = t.toCharArray();
-        
-        
-        for(int i=0;i<s.length();i++){
-           Alphabet[s.charAt(i)-'a']++;                     
+
+        if(s.length() != t.length()) return false;
+
+        int[] alp = new int[26];
+
+        for(int i = 0; i < s.length(); i++){
+            
+            alp[s.charAt(i) - 'a']++;
+            alp[t.charAt(i) - 'a']--;    
+        }        
+
+        for(int i = 0; i < 26; i++){
+            
+            if(alp[i] != 0) return false;
         }
         
-        for(int j=0;j<t.length();j++){
-            Alphabet[t.charAt(j) - 'a']--;
-        }
-        
-       for(int k=0;k<26;k++){
-           if(Alphabet[k] !=0) return false;
-       }
-                
-       return true;
-        
+        return true;
     }
 }
