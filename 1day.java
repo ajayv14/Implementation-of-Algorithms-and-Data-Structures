502965335e83eb324c8e33cf8b4a9906e9b4ef78
Ajay <ajay.v14@gmail.com>
2025-04-23 21:42:56 -0700

    Updated with more algo templates

--- 1day.java
+++ 1day.java
@@ -579,9 +579,6 @@ class QuickSelect {
 }
 
 
-
-
-
 class TopologicalSort {
 
 
@@ -643,5 +640,462 @@ class TopologicalSort {
 
     }
 
+
+}
+
+
+
+
+class BinarySearch {
+
+
+    public int search(int[] arr, int key) {
+
+        int start = 0, end = arr.length - 1;
+
+        while (start <= end) {
+
+            int midIndex = start + (end - start) / 2; // to prevent overflow
+
+
+            // search on left half
+            if (key < arr[midIndex]) {
+                end = midIndex - 1;
+            }
+
+            // search in right half
+            else if (key > arr[midIndex]) {
+                start = midIndex + 1;
+            } else if (key == arr[midIndex]) return midIndex;
+
+        }
+
+        return -1;
+    }
+}
+
+
+
+// LC 153 Find Minimum in Rotated Sorted Array
+
+public class FindMInRotatedSortedArr {
+
+    // Using minimization template
+
+
+    public int findMin(int[] nums) {
+
+
+        int low = -1, high = nums.length - 1;
+
+        while (low + 1 < high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] <= nums[nums.length - 1]) high = mid;
+
+            else low = mid;
+
+        }
+
+        return nums[high];
+
+    }
+
+
+}
+
+
+
+
+public class CuttingRibbons {
+
+    // LC 
+
+    // Binary search optimized 
+    public int maxLength(int[] ribbons, int k) {
+
+        // k is not the length, but number of ribbons   
+
+        // min len is 1, so left boundary is 1, right boundary ?? below
+        int max = 0;
+
+        for (int i = 0; i < ribbons.length; i++) {
+
+            max = Math.max(max, ribbons[i]);
+        }
+
+        // Now length of ribbon is btw 1 and max. We optimize this value by binary search.
+
+        // Binary search
+        // Maximization problem
+        // min length = 1 and max len = max
+        // invalid range to begin with 
+        // in maximization problem return lo       
+        int low = 0, high = max + 1;
+
+        while (low + 1 < high) {
+
+            int mid = low + (high - low) / 2;
+
+            boolean isPossible = isMaxLenPossible(ribbons, mid, k);
+
+            if (isPossible) low = mid;
+
+            else high = mid;
+        }
+
+        return low;
+    }
+
+    private boolean isMaxLenPossible(int[] ribbons, int len, int k) {
+
+        int sum = 0;
+
+        for (int r = 0; r < ribbons.length; r++) {
+
+            sum += Math.floor(ribbons[r] / len);
+        }
+
+        return sum >= k ? true : false;
+
+    }
+
+
+    // Brute force  
+    public int maxLength2(int[] ribbons, int k) {
+
+        // k is not the length, byt number of ribbons   
+
+
+        // min len is 1, so left boundary is 1, right boundary ?? below
+        int max = 0;
+
+        for (int i = 0; i < ribbons.length; i++) {
+
+            max = Math.max(max, ribbons[i]);
+        }
+
+        // length of ribbons possible
+        for (int l = max; l > 0; l--) {
+
+            int sum = 0;
+
+            for (int j = 0; j < ribbons.length; j++) {
+
+                sum += Math.floor(ribbons[j] / l);
+            }
+
+            if (sum >= k) return l;
+
+        }
+
+        return 0;
+
+    }
+}
+
+
+// LC 34 https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+class FindFirstAndLast {
+
+    /**
+     * Use binary search minimization and maximization template.
+     */
+    public int[] searchRange(int[] nums, int target) {
+
+        int[] res = new int[2];
+
+        res[0] = findFirst(nums, target);
+        res[1] = findLast(nums, target);
+
+        return res;
+
+    }
+
+    private int findFirst(int[] nums, int target) {
+
+        int low = -1;
+        int high = nums.length;
+
+        while (low + 1 < high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] >= target) high = mid;
+
+            else low = mid;
+        }
+
+        if (high == nums.length || nums[high] != target) return -1;
+
+        return high;
+    }
+
+    private int findLast(int[] nums, int target) {
+
+        int low = -1;
+        int high = nums.length;
+
+        while (low + 1 < high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] <= target) low = mid;
+
+            else high = mid;
+        }
+
+        if (low == -1 || nums[low] != target) return -1;
+
+        return low;
+
+    }
+
+}
+
+
+
+class MonotonicStack1 {
+
+    public int[] asteroidCollision(int[] asteroids) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < asteroids.length; i++) {
+
+            boolean destroyed = false;
+
+            // +ve spin meets negative spin
+            while (!stack.isEmpty() && (stack.peek() > 0 && asteroids[i] < 0)) {
+
+                // Note : After exploding the smaller one, Surviving asteroid doesn't lose weight
+
+                // stack top is smaller, hence will explode and put the next asterioid in line
+
+                if (Math.abs(stack.peek()) < Math.abs(asteroids[i])) {
+                    stack.pop();
+                    continue;
+                }
+
+                // stack top is equal to aster. Both will explods
+                else if (Math.abs(stack.peek()) == Math.abs(asteroids[i])) {
+                    stack.pop();
+                }
+
+                destroyed = true;
+                break;
+
+
+            }
+
+            if (!destroyed) stack.push(asteroids[i]);
+        }
+
+
+        // Build result from stack
+        int res[] = new int[stack.size()];
+        int k = res.length - 1;
+        while (!stack.isEmpty()) {
+
+            res[k--] = stack.pop();
+        }
+
+        return res;
+    }
+}
+
+
+public class MonotonicStack2 {
+  
+
+
+    // Monotonic stack 
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int[] res = new int[temperatures.length];
+
+        Stack<Integer> stack = new Stack<>();
+
+
+        for (int i = 0; i < temperatures.length; i++) {
+
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+
+                int idx = stack.pop();
+
+                res[idx] = i - idx;
+            }
+
+            stack.push(i);
+
+        }
+
+        return res;
+
+    }
+
+}
+
+
+
+
+
+
+/*
+                   s 0 ---------- e 3
+                           st 2-----------en 6            
+
+                Now, intersection is between st 2 and e 3.
+
+                if st 2 < e 3, then merge.
+
+                To calc intersection points : 
+
+                  max(s 0 and st 2) -> st 2
+                  min(e 3 and en 6) -> e 3 
+            */
+
+// Time O(n log n) space : O(n)         
+
+public class MergeIntervals {
+
+    /*Solve using Priority Queue*/
+
+    public int[][] merge(int[][] intervals) {
+
+        // To store intermediate resut as a list instead of int[][] array
+        List<int[]> res = new ArrayList<>();
+
+        // Sorted order based on start time - As opposed to sorting the array in place
+        PriorityQueue<int[]> pq = new PriorityQueue<>((n1, n2) -> n1[0] - n2[0]);
+
+
+        for (int[] interval : intervals) {
+            pq.add(interval);
+        }
+
+        while (pq.size() > 1) {
+
+            int[] n1 = pq.remove();
+            int[] n2 = pq.remove();
+
+            // Merge condition   
+            if (n2[0] <= n1[1]) {
+
+                int end = n1[1] > n2[1] ? n1[1] : n2[1];
+
+                pq.add(new int[]{n1[0], end}); // Start is n1[0] as we know it is sorted by start time in PQ
+            } else {
+                res.add(n1);
+                pq.add(n2);
+            }
+        }
+
+        if (!pq.isEmpty()) {
+
+            res.add(pq.remove());
+        }
+
+
+        return res.toArray(new int[res.size()][]);
+
+
+    }
+
+    /*int[][] result = new int[res.size()][2];
+
+        int idx = 0;
+
+        for(int[] r : res){
+            result[idx][0] = r[0];
+            result[idx][1] = r[1];
+            idx++;        
+        }
+
+        return result;
+        */
+
+/*
+ * 
+Time Complexity Analysis
+    The time complexity of the provided merge function can be broken down into the following components:
+        Adding intervals to the priority queue: O(n log n)
+        Removing nodes from the priority queue and merging intervals: O(n log n)
+        Converting the result list to an array: O(n)
+    Therefore, the overall time complexity is O(n log n) due to the priority queue operations.
+
+    Space Complexity Analysis
+    The space complexity of the provided merge function can be broken down into the following components:
+        Storing intervals in the priority queue: O(n)
+        Storing merged intervals in the result list: O(n)
+        Additional space for the array conversion: O(n)
+    
+    Therefore, the overall space complexity is O(n), where n is the number of intervals.
+  * - Meta ai
+ */
+
+    public int[][] mergeWithSort(int[][] intervals) {
+
+        /*logic - sort the intervals with the start time value, then if end index of current >= start index of next, merge*/
+
+
+        if (intervals.length <= 1) return intervals;
+
+
+        // list of array to hold result (easy to manipulate)
+        List<int[]> res = new ArrayList<>();
+
+
+        /* sort based on the interval start value*/
+
+        Arrays.sort(intervals, (arr1, arr2) -> Integer.compare(arr1[0], arr2[0]));
+
+
+        int[] prev = intervals[0]; // first set of value
+        res.add(prev);
+
+        for (int[] interval : intervals) {
+
+            int prevStart = prev[0];
+            int prevEnd = prev[1];
+
+            int currentStart = interval[0];
+            int currentEnd = interval[1];
+
+            if (prevEnd >= currentStart) {   // condition to merge
+
+                prev[1] = Math.max(prevEnd, currentEnd);  // end value of merged set
+
+            } else {
+
+                /*update prev set value*/
+                prev = interval;  // set prev value to current
+                res.add(prev); // to continue the cycle
+            }
+
+        }
+
+        return res.toArray(new int[res.size()][]);
+    }
+
+
+    /*test*/
+    public static void main(String args[]) {
+
+        //input
+        int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}}; // o/p [[1,6],[8,10],[15,18]]
+
+
+        MergeIntervals obj = new MergeIntervals();
+        int[][] result = obj.merge(intervals);
+
+        /*print output*/
+        for (int[] res : result) {
+            System.out.println("[" + res[0] + ", " + res[1] + "]");
+        }
+
+    }
 
 }
