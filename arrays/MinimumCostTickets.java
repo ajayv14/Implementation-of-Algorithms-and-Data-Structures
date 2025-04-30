7c8f5717d75612236051a658f10a3ad25f02c7ba
Ajay <ajay.v14@gmail.com>
2025-04-29 18:56:36 -0700

    Updated 1day scroll

--- arrays/MinimumCostTickets.java
+++ arrays/MinimumCostTickets.java
@@ -1,25 +1,32 @@
-package com.app.arrays;
+
 
 public class MinimumCostTickets {
 
+    /*
+     * There are 2 arrays which denote departing and returning flights with the respective indexes being 
+     * time and the values of the array being the cost it takes for the flight.
+     *  Return the minimum cost for a round trip provided the return flight can only be taken at a time post departing flight time 
+     * (i.e if departing at time i, one can catch a returning flight only from time (i+1) onwards). 
+     * For eg departing = [1,2,3,4] and returning = [4,3,2,1], 
+     * the minimum cost for round trip will be 2 i.e departing[0] + returning[3]. Solve this is O(n) time
+     * 
+     */
+
+     public int findMinCost(int[] departing, int[] returning) {
+
+        int n = departing.length;
+        int minReturn = Integer.MAX_VALUE;
+        int minCost = Integer.MAX_VALUE;
     
-
-    public int findMinCost(int[] departing, int[] returning) {
-
-        int minDepCost = departing[0];
-        int minTripCost = Integer.MAX_VALUE;
-        
-
-        for(int i = 1; i < departing.length; i++){
-
-            minDepCost = Math.min(minDepCost, departing[i]);
-
-            minTripCost = Math.min(minTripCost, minDepCost + returning[i]); // i + 1 -> can't return on same day
-
+        // Traverse backwards from the end to track minimum returning[i] for j > i
+        for (int i = n - 1; i > 0; i--) {
+            // Update minReturn before using it, since i is for departing
+            minReturn = Math.min(minReturn, returning[i]);
+            int tripCost = departing[i - 1] + minReturn;
+            minCost = Math.min(minCost, tripCost);
         }
-
-        return minTripCost;
-        
+    
+        return minCost;
     }
     
     public static void main(String[] args) {
