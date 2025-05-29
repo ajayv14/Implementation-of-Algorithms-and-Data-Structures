f079146ddbb38babdfc280f014c6c1a4cf352f5a
Ajay <ajay.v14@gmail.com>
2025-05-28 21:19:23 -0700

    Added visual soln for bottom up dp coin change 2

--- dynamicprogramming/ClimbStairs.java
+++ dynamicprogramming/ClimbStairs.java
@@ -1,4 +1,3 @@
-package com.app.dynamicprogramming;
 
 class ClimbStairs{
 
@@ -32,16 +31,22 @@ class ClimbStairs{
       dp[0]=cost[0];
       dp[1]=cost[1];
       
-      for(int i=2;i<cost.length;i++){
-          dp[i]=cost[i] + Math.min(dp[i-1],dp[i-2]);
+      for(int i = 2;i < cost.length; i++){
+          
+            dp[i] = cost[i] + Math.min(dp[i - 1],dp[i - 2]);
       }        
-      return Math.min(dp[cost.length-1],dp[cost.length-2]);
+
+      return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
       
   }
       
       public static void main(String[] args){
+        
+        
         ClimbStairs obj = new ClimbStairs();
-            System.out.println(obj.climbStairs(3));
-      }
+        
+        System.out.println(obj.climbStairs(3));
+      
+    }
 
 }
