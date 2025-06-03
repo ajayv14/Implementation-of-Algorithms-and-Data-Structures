fa3975c15c91d952143165c0e7ebaf70977be164
Ajay <ajay.v14@gmail.com>
2025-06-02 23:24:12 -0700

    Addedn binary search based soln for koko eating bananas.

---
+++ binarysearch/KokoEatingBananas.java
@@ -0,0 +1,48 @@
+class KokoEatingBananas {
+
+    public int minEatingSpeed(int[] piles, int h) {
+
+
+        // Binary search minimization
+
+        int max = 0; // 1 banana per hour
+
+        for(int num : piles){
+
+            max = Math.max(max, num);
+        }
+
+        // Now we need to optimize num of banana per hour from 1 - max;
+        // Can use for loop to test 1 .....max, but can use binary search to optimize 
+
+        int low = 0, high = max;
+
+        while(low + 1 < high){
+
+            int mid = low + (high - low) / 2;
+
+            boolean yes = canEat(mid, piles, h);
+
+            if(yes) high = mid;
+
+            else low = mid;
+        }    
+
+        return high;
+
+    }
+
+
+    private boolean canEat(int mid, int[] piles, int maxHours){
+
+        int time = 0;
+
+        for(int i = 0; i < piles.length; i++){
+
+            time += Math.ceil((double)piles[i]/mid);
+        }
+
+        return time <= maxHours;
+
+    }
+}
